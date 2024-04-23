58f2f8dc88cb2f8ebfe6701306f8d2a33ef82389
MarkMikhail1 <markmikhail4@gmail.com>
2024-04-23 13:47:48 +1000

    Refactor MongoDB connection configuration in db.config.ts

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,9 +1,27 @@
-import * as mongoose from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const uri: string = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-const options: object = {maxPoolSize: 10}
+// Constructing the connection string using environment variables
+console.log(process.env.DB_HOST, process.env.DB_PORT, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_NAME);
 
-export const dbConnection = async () => {return mongoose.connect(uri, options)}
+const uri: string = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+// Connection options
+const options: mongoose.ConnectOptions = {
+    maxPoolSize: 10, // Limits the number of connections in the MongoDB connection pool
+    authSource: 'admin' // Specifies the database that should be used to authenticate (if required)
+};
+
+// Async function to establish a database connection
+export const dbConnection = async () => {
+    try {
+        await mongoose.connect(uri, options);
+        console.log("Successfully connected to the database.");
+    } catch (error) {
+        console.error("Database connection failed:", error);
+        process.exit(1); // Exit the process with a failure code if the connection fails
+    }
+};
+
 
