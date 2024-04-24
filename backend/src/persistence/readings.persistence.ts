31a4971dd5c244a936baf677f96c04cce060a7f2
MarkMikhail1 <markmikhail4@gmail.com>
2024-04-25 09:35:15 +1000

    Refactor readImmediateReadings function to accept an end time parameter

--- backend/src/persistence/readings.persistence.ts
+++ backend/src/persistence/readings.persistence.ts
@@ -4,23 +4,26 @@ import { SensorReadingModel } from "../models/readings.model";
  * Interface for the function to fetch immediate sensor readings
  * 
  * @param startTime - The start time from which to fetch sensor readings.
+ * @param endTime - The end time until which to fetch sensor readings.
  * @returns An array of SensorReading documents.
  */
 interface IReadImmediateReadings {
-    (startTime: Date): Promise<any[]>;
+    (startTime: Date, endTime: Date): Promise<any[]>;
 }
 
 /**
- * Fetches immediate sensor readings from the database starting from the specified time.
+ * Fetches immediate sensor readings from the database within the specified time range.
  * 
  * @param startTime - The start time from which to fetch sensor readings.
+ * @param endTime - The end time until which to fetch sensor readings.
  * @returns An array of SensorReading documents.
  */
-export const readImmediateReadings: IReadImmediateReadings = async (startTime) => {
-    // Filter to fetch sensor readings starting from the specified startTime
+export const readImmediateReadings: IReadImmediateReadings = async (startTime, endTime) => {
+    // Filter to fetch sensor readings within the specified time range
     const filter = {
-        time: { $gte: startTime }
+        time: { $gte: startTime, $lt: endTime }
     };
 
     return await SensorReadingModel.find(filter).exec();
 };
+
