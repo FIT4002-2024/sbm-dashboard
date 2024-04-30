cd5c769207179daaa6a1fb521ccecea81fc4dc98
Huynh Phuc Nguyen Nguyen <hngu0112@student.monash.edu>
2024-04-30 14:56:15 +1000

    addding error dialog popup template so we can add content later

--- sbm-dashboard/src/components/Popup.tsx
+++ sbm-dashboard/src/components/Popup.tsx
@@ -5,18 +5,24 @@ import DialogContent from "@mui/material/DialogContent";
 
 interface PopupProps {
   title: string;
-  children: React.ReactNode;
-  openPopup: () => void;
+  popupContent: React.ReactNode;
+  openPopup: boolean;
   setOpenPopup: (value: boolean) => void;
 }
 
 const Popup: React.FC<PopupProps> = (props: PopupProps) => {
-  const { title, children, openPopup, setOpenPopup } = props;
+  const { title, popupContent, openPopup, setOpenPopup } = props;
   return (
     <div>
-      <Dialog open={{ openPopup }}>
+      <Dialog open={openPopup} style={{ textAlign: "center" }}>
         <DialogTitle>{title}</DialogTitle>
-        <DialogContent>{children}</DialogContent>
+        <DialogContent dividers style={{ width: "300px" }}>
+          {popupContent}
+          <br />
+          <div style={{ marginTop: "5px" }}>
+            <button onClick={() => setOpenPopup(false)}>Cancel</button>
+          </div>
+        </DialogContent>
       </Dialog>
     </div>
   );
--- sbm-dashboard/src/pages/OperationsPage.tsx
+++ sbm-dashboard/src/pages/OperationsPage.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import Popup from "../components/Popup";
 function OperationsPage() {
-    return <div>Content for Operations</div>;
+  const [openPopup, setOpenPopup] = useState(false);
+
+  return (
+    <div>
+      Content for Operations
+      <button onClick={() => setOpenPopup(true)}>Open Popup</button>
+      <Popup
+        title="Popup Title"
+        openPopup={openPopup}
+        setOpenPopup={(value: boolean) => setOpenPopup(value)}
+        popupContent={<div>Popup Content</div>}
+      ></Popup>
+    </div>
+  );
 }
 
 export default OperationsPage;
