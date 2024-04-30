daea14cdb05ab9974cf6ce5597d4787cda364a15
Nick Nguyen <114848671+mcnguyen963@users.noreply.github.com>
2024-04-30 17:38:29 +1000

    Revert "Creating a pop up dialog component and testing button for display on the front-end"

--- sbm-dashboard/src/pages/OperationsPage.tsx
+++ sbm-dashboard/src/pages/OperationsPage.tsx
@@ -1,29 +1,5 @@
-import { useState } from "react";
-import Popup from "../components/Popup";
-import SensorPopup from "../components/SensorPopup";
 function OperationsPage() {
-  const [openPopup, setOpenPopup] = useState(false);
-
-  return (
-    <div>
-      Content for Operations
-      <button onClick={() => setOpenPopup(true)}>Open Popup</button>
-      <Popup
-        title="Sensor Value Exceeded"
-        openPopup={openPopup}
-        setOpenPopup={(value: boolean) => setOpenPopup(value)}
-        popupContent={
-          <SensorPopup
-            name="Temperature Sensor"
-            value={10}
-            date="10/31/2024"
-            time="12:00"
-            suggestionAction="Check the sensor"
-          ></SensorPopup>
-        }
-      ></Popup>
-    </div>
-  );
+    return <div>Content for Operations</div>;
 }
 
 export default OperationsPage;
