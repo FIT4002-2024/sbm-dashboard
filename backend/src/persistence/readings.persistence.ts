1b384bc1f61639c17daf31ce01ec8a9b5fcd783d
Baaset Moslih <s.a.baaset.moslih@gmail.com>
2024-05-03 02:22:42 +1000

    refac: reverted streamImmediate code to not hardcode and populate db itself

--- backend/src/persistence/readings.persistence.ts
+++ backend/src/persistence/readings.persistence.ts
@@ -2,31 +2,32 @@ import { SensorReadingModel } from "../models/readings.model";
 import {start} from "repl";
 
 /**
- * Interface for the function to fetch immediate sensor readings
- * 
- * @param startTime - The start time from which to fetch sensor readings.
- * @param endTime - The end time until which to fetch sensor readings.
- * @returns An array of SensorReading documents.
+ * Connection to database that returns all sensor readings for the current minute
+ *
+ * @return: array of SensorReading documents
  */
 interface IReadImmediateReadings {
-    (startTime: Date, endTime: Date): Promise<any[]>;
+    (): Promise<any[]>
 }
 
-/**
- * Fetches immediate sensor readings from the database within the specified time range.
- * 
- * @param startTime - The start time from which to fetch sensor readings.
- * @param endTime - The end time until which to fetch sensor readings.
- * @returns An array of SensorReading documents.
- */
-export const readImmediateReadings: IReadImmediateReadings = async (startTime, endTime) => {
-    // Filter to fetch sensor readings within the specified time range
-    const filter = {
-        time: { $gte: startTime, $lt: endTime }
+export const readImmediateReadings: IReadImmediateReadings = async () => {
+
+    // calculate the current minute
+    const now: Date = new Date();
+    const MS_PER_S: number = 1000;
+    const MS_IN_MIN: number = MS_PER_S * 60
+    const excessTime: number = now.getMilliseconds() + now.getSeconds() * MS_PER_S;
+    const currMinute: number = now.valueOf() - excessTime.valueOf();
+
+    // filter out all records not within the current minute
+    const filter: Object = {
+        time: {
+            $gte: new Date(currMinute), $lt: new Date(currMinute + MS_IN_MIN)
+        }
     };
 
-    return await SensorReadingModel.find(filter).exec();
-};
+    return await SensorReadingModel.find(filter).exec()
+}
 
 /**
  * Connection to database that returns all sensor readings from 'scope' period of time
@@ -41,15 +42,6 @@ interface IReadTimeSeriesReadings {
     (sensorId: string, scope: string): Promise<any[]>
 }
 
-/**
- * Connection to database that returns all sensor readings from 'scope' period of time
- * until now
- *
- * @sensorId: the UUID for the sensor whose data is being requested
- * @param: a string indicating the scope either hour, day or week
- *
- * @return: array of SensorReading documents
- */
 export const readTimeSeriesReadings: IReadTimeSeriesReadings = async (sensorId: string, scope: string) => {
 
     // calculate the times for the scope:
