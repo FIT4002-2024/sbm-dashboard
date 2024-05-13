14405cb92b57035e80f4b48412f0f22b7e66cfa5
MarkMikhail1 <markmikhail4@gmail.com>
2024-05-13 14:02:44 +1000

    Refactor store.ts to use createSlice for sensorProps state management

--- sbm-dashboard/src/store/store.ts
+++ sbm-dashboard/src/store/store.ts
@@ -1,8 +1,41 @@
 import { configureStore } from '@reduxjs/toolkit'
-import sensorSlice from './sensors'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { SensorProps } from './SensorInstance';
 
-export default configureStore({
+interface SensorState {
+    sensorProps: SensorProps;
+}
+
+const initialState: SensorState = {
+    sensorProps: {
+        factoryName: '',
+        sensorId: '',
+        sensorType: '',
+        currentValue: 0,
+        unit: '',
+        highValue: 0,
+        lowValue: 0,
+    },
+};
+
+const sensorSlice = createSlice({
+    name: 'sensors',
+    initialState,
+    reducers: {
+        setSensorProps(state, action: PayloadAction<SensorProps>) {
+            state.sensorProps = action.payload;
+        },
+    },
+});
+
+export const { setSensorProps } = sensorSlice.actions;
+
+const store = configureStore({
     reducer: {
-        sensors: sensorSlice
-    }
-}) 
+        sensors: sensorSlice.reducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
