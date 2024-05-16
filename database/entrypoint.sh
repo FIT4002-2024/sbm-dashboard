41f70c9a3aac3dfd4b9730e986f397888c41d3ca
Joshua Ong <joshuaong2000@gmail.com>
2024-05-16 18:29:09 +1000

    Docs and updated for cluster

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -1,19 +1,30 @@
 set -eu 
 
 main() {
+    local is_cluster_setup=1
+    if [ "${IS_REPLICA:--1}" = -1 ]; then
+        is_cluster_setup=0
+    fi 
+
     local replica_flag=""
-    if [ "${IS_CLUSTER_SETUP:-0}" = "1" ]; then 
+    if [ "${is_cluster_setup}" = 1 ]; then 
         replica_flag="--replSet sbm" 
     fi
     mongod ${replica_flag} --bind_ip_all --port ${MONGODB_PORT:-27017} &
     sleep 30
 
-    if [ "${IS_CLUSTER_SETUP:-0}" = "1" ]; then 
-        mongosh -f ./initiate_replication.js || true
+    if [ "${is_cluster_setup}" = 1 ]; then 
+        if [ "${IS_REPLICA}" = 0 ]; then
+            mongosh -f ./initiate_replication.js || true
+            sh ./tmp/add_test_users.sh || true
+            python3 ./mock_data_gen.py -g 30 -s day -e day
+        else
+            sleep 10 
+        fi
+    else
+        sh ./tmp/add_test_users.sh || true
+        python3 ./mock_data_gen.py -g 30 -s day -e day
     fi
-
-    sh ./tmp/add_test_users.sh || true
-    python3 ./mock_data_gen.py -g 30 -s day -e day
 
     tail -f /dev/null
 }
--- database/initiate_replication.js
+++ database/initiate_replication.js
@@ -1,8 +1,20 @@
 rs.initiate({
     _id: "sbm",
     members: [
-        { _id: 0, host: "initial_primary:27017" },
-        { _id: 1, host: "replica_1:27018" },
-        { _id: 2, host: "replica_2:27019" }
+        { 
+            _id: 0,
+            host: "initial_primary:27017",
+            priority: 1
+        },
+        { 
+            _id: 1, 
+            host: "replica_1:27018",
+            priority: 0
+        },
+        { 
+            _id: 2, 
+            host: "replica_2:27019",
+            priority: 0
+        }
     ]
 }) 
