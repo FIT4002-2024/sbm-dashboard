7fe1a7882dde2d0238c760181cc6ee6f0f05dabe
MarkMikhail1 <markmikhail4@gmail.com>
2024-05-16 14:08:41 +1000

    Update TimeSeriesView page with ChartJS and add sensor details modal

--- sbm-dashboard/src/pages/TimeSeriesView.tsx
+++ sbm-dashboard/src/pages/TimeSeriesView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
@@ -8,63 +8,82 @@ import Button from '@mui/material/Button';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { Tooltip } from 'antd';
-import { InfoCircleOutlined } from '@ant-design/icons';
-import { Line } from 'react-chartjs-2';
+import { PlusOutlined, InfoCircleOutlined, FilterOutlined } from '@ant-design/icons';
 import { 
-    Chart as ChartJS,
-    LineElement,
-    PointElement,
-    CategoryScale,
-    LinearScale,
-    ToolTip,
-    Legend
+    Chart,
 } from 'chart.js'
-
 
 
 const TimeSeriesView: React.FC = () => {
     const { sensorId } = useParams<{ sensorId: string }>();
     const sensorProps = useSelector((state: RootState) => state.sensors.sensorProps);
-    const [sensorName, setSensorName] = useState<string>('');
     const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
-    const [data, setData] = useState<number[]>([]);
-    const [labels, setLabels] = useState<string[]>([]);
     const chartWidth = window.innerWidth * 0.8; // 80% of window width
     const chartHeight = window.innerHeight * 0.6;
+    const chartRef = useRef(null);
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const scope = 'day';
+    //         try{
+    //             const response = await fetch(`/api/sensors/stream-timeseries/${sensorId}/${scope}`);
+    //             const text = await response.text();
+    //             console.log('Response text:', text);
+
+    //             if (!response.ok) {
+    //                 console.error('Fetch request failed:', response);
+    //                 return;
+    //             }
+        
+    //             const data = JSON.parse(text);
+    //             console.log('DATA: ', data);
+        
+    //             if (Array.isArray(data)) {
+    //                 setChartData({
+    //                     labels: data.map(reading => reading.timestamp),
+    //                     datasets: [{
+    //                         label: 'Sensor Readings',
+    //                         data: data.map(reading => reading.value),
+    //                         fill: false,
+    //                         backgroundColor: 'rgb(75, 192, 192)',
+    //                         borderColor: 'rgba(75, 192, 192, 0.2)',
+    //                     }],
+    //                 });
+    //             }
+    //         } catch (error) {
+    //             console.error('Fetch request failed:', error);
+    //         }
+    //     };
+
+    //     fetchData();
+    // }, [sensorId]);
 
     useEffect(() => {
-        // ...
-    
-        const ctx = document.getElementById('myChart').getContext('2d');
-        new ChartJS(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Sensor Readings',
-                    data: data,
-                    fill: false,
-                    backgroundColor: 'rgb(75, 192, 192)',
-                    borderColor: 'rgba(75, 192, 192, 0.2)',
-                }],
-            },
-            options: {
-                scales: {
-                    x: {
-                        type: 'time',
-                        time: {
-                            unit: 'second'
-                        }
-                    },
-                    y: {
-                        type: 'linear'
+        if (chartRef && chartRef.current) {
+            const chartInstance = new Chart(chartRef.current, {
+                type: 'line',
+                data: {
+                    labels: [],
+                    datasets: [{
+                        label: '',
+                        data: [],
+                        fill: false,
+                        backgroundColor: 'rgb(75, 192, 192)',
+                        borderColor: 'rgba(75, 192, 192, 0.2)',
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
                     }
                 }
-            }
-        });
-    
-        // ...
-    }, [sensorId]);
+            });
+        }
+    }, []);
 
     if (!sensorProps) {
         return <div>Loading...</div>;
@@ -84,11 +103,28 @@ const TimeSeriesView: React.FC = () => {
                     </Link>
                 </div>
                 <div style={{position: 'absolute', top: '100px', left: '10px'}}>
-                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px'}}>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '20px'}}>
                         <h3 style={{ marginRight: '10px' }}>Sensor: {sensorId}</h3>
                         <Tooltip title="More Info" onClick={() => setModalIsOpen(true)}>
                             <InfoCircleOutlined />
                         </Tooltip>
+                        <Button 
+                            variant="contained" 
+                            color="primary" 
+                            startIcon={<PlusOutlined />} 
+                            style={{ marginLeft: '500px' }}
+                        >
+                            Add
+                        </Button>
+                        <div style={{ marginLeft: '10px' }}>
+                        <Button 
+                            variant="contained" 
+                            startIcon={<FilterOutlined />} 
+                            sx={{ bgcolor: 'grey.500', color: 'white' }}
+                        >
+                            Scope
+                        </Button>
+                        </div>
                     </div>
                     <Dialog open={modalIsOpen} onClose={() => setModalIsOpen(false)}>
                         <DialogTitle>Sensor Details</DialogTitle>
@@ -107,8 +143,8 @@ const TimeSeriesView: React.FC = () => {
                     </Dialog>
                 </div>
                 <div style={{ marginTop: '100px' }}>
-                <canvas id="myChart" width={chartWidth} height={chartHeight}></canvas>
-            </div>
+                    <canvas ref={chartRef} width={chartWidth} height={chartHeight} />
+                </div>
             </div>
         </div>
     );
